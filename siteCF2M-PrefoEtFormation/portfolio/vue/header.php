16c24d2bbfdf7357220e8f7a4c0895e4668cde92
thomas-ayissi <thomas8sea@gmail.com>
2021-12-28 23:33:39 +0100

    SauvegardeAvantModificationsGlobalCF2MMar28Dec2021

---
+++ siteCF2M-PrefoEtFormation/portfolio/vue/header.php
@@ -0,0 +1,28 @@
+
+<body>
+<br>
+<br>
+<!--div id="logo"><a href="./?pg=logo_thomas"></a></div-->
+<div id="logo1"><img id="logo2" src="logo_thomas_ok.svg" alt="logo_thomas"></div>
+
+
+<div id="bas">
+
+</div>
+<br>
+<br>
+<nav id="header">
+    <div id="menu">
+         <h3></h3>
+        <ul>
+            <li><a href="./">Accueil</a></li>
+            <li><a href="./?pg=gallery">Galerie</a></li>
+            <li><a href="./?pg=liaisons">Liens</a></li>
+            <li><a href="./?pg=tutorials">Tutoriels</a></li>
+            <li><a href="./?pg=contacts">Contact</a></li>
+            <li><a href="./?pg=administration">Admin</a></li>
+            <li><a href="./?pg=CV">CV</a></li>
+        </ul>
+    </div>
+</nav>
+</body>
