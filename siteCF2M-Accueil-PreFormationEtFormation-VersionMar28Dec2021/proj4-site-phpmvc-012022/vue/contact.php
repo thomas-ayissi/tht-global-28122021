dda5ae8b73aaf89cd59b0e42f839af61d69c618d
thomas-ayissi <thomas8sea@gmail.com>
2022-01-02 21:40:43 +0100

    Déplacement de tous les sites dans le dossier du nouveau site Préformation. Nous sommes partie de la branche précédent que je n'ai pas sauvegardé au cas où il faudrait "undo"

---
+++ siteCF2M-Accueil-PreFormationEtFormation-VersionMar28Dec2021/proj4-site-phpmvc-012022/vue/contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link type="text/css" rel="stylesheet" href="../decor/style.css" />
+</head>
+
+
+<body id="global">
+<div class="container">
+
+
+
+    <!-- inclusion de l entête du site -->
+    <?php include_once "header.php"; ?>
+    <br>
+    <br>
+    <h1><?php include_once "titre_body.php"; ?></h1>
+
+
+
+    <h2>Contactez nous</h2>
+    <form>
+        <div class=" ">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+            <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+        </div>
+        <div class=" ">
+            <label for="message" class="form-label">Votre message</label>
+            <textarea class="form-control" placeholder="Exprimez vous" id="message" name="textarea" row="4" cols="20"></textarea>
+        </div>
+        <button type="submit" class=" ">Envoyer</button>
+    </form>
+    <br />
+</div>
+
+     <br>
+     <br>
+     <br>
+    <!-- inclusion du bas de page du site -->
+    <?php include_once "pied_de_page.php"; ?>
+</body>
+</html>
+
---
+++ siteCF2M-Accueil-PreFormationEtFormation-VersionMar28Dec2021/proj4-site-phpmvc-012022/vue/liens.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liens</title>
+    <link type="text/css" rel="stylesheet" href="../decor/style.css" />
+</head>
+
+
+<body id="global">
+<div class="container">
+
+
+
+
+    <!-- inclusion de l'entête du site -->
+    <?php include_once "header.php"; ?>
+    <br>
+    <br>
+    <!-- inclusion de du titre du body du site -->
+    <h1><?php include_once "titre_body.php"; ?></h1>
+
+
+    <br>
+    <br>
+    <br>
+    <!-- inclusion du bas de page du site -->
+    <?php include_once "pied_de_page.php"; ?>
+
+
+</body>
+</html>
